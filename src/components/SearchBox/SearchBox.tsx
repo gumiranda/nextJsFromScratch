c3c35aabfaa82382d9a3c0a05b43bae8cc35b007
gumiranda <imartytk@hotmail.com>
2021-03-26 21:36:52 -0300

    resizing page

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,44 +1,126 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, Space, Select } from 'antd';
 
 const { Search } = Input;
 const { Option } = Select;
-
-const SearchBox = ({
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+function SearchBox({
   onSearch, fieldsQuery, defaultField, handleChange, handleChangeOrder, defaultTypeSort, typeSortOptions, handleChangeTypeOrder,
-}) => (
-  <>
-    <div className="search-box">
-      <Space style={{ margin: '2rem' }}>
-        <h4>Search by</h4>
-        <Select defaultValue={defaultField} style={{ width: 120 }} onChange={handleChange}>
-          {fieldsQuery.map((field) => (
-            <Option value={field}>{field}</Option>
-          ))}
-        </Select>
-        <Search
-          placeholder="input search text"
-          allowClear
-          enterButton="Search"
-          size="large"
-          onSearch={onSearch}
-        />
-        <h4>Order by</h4>
-        <Select defaultValue={defaultField} style={{ width: 120 }} onChange={handleChangeOrder}>
-          {fieldsQuery.map((field) => (
-            <Option value={field}>{field}</Option>
-          ))}
-        </Select>
-        <h4>Type order</h4>
-        <Select defaultValue={defaultTypeSort} style={{ width: 120 }} onChange={handleChangeTypeOrder}>
-          {typeSortOptions.map((field) => (
-            <Option value={field}>{field}</Option>
-          ))}
-        </Select>
-      </Space>
-    </div>
-  </>
-);
+}) {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    <>
+      {windowDimensions.width > 477 ? (
+        <>
+          <div className="search-bar">
+
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Search by</p>
+              <Select defaultValue={defaultField} onChange={handleChange}>
+                {fieldsQuery.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+            <Space style={{ margin: '1rem' }}>
+              <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+              />
+            </Space>
+
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Order by</p>
+              <Select defaultValue={defaultField} onChange={handleChangeOrder}>
+                {fieldsQuery.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Type order</p>
+              <Select defaultValue={defaultTypeSort} onChange={handleChangeTypeOrder}>
+                {typeSortOptions.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="search-box">
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Search by</p>
+              <Select defaultValue={defaultField} onChange={handleChange}>
+                {fieldsQuery.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+          </div>
+
+          <div className="search-bar">
+
+            <Space style={{ margin: '1rem' }}>
+              <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+              />
+            </Space>
+          </div>
+          <div className="search-box">
+
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Order by</p>
+              <Select defaultValue={defaultField} onChange={handleChangeOrder}>
+                {fieldsQuery.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+            <Space style={{ margin: '1rem' }}>
+
+              <p>Type order</p>
+              <Select defaultValue={defaultTypeSort} onChange={handleChangeTypeOrder}>
+                {typeSortOptions.map((field) => (
+                  <Option value={field}>{field}</Option>
+                ))}
+              </Select>
+            </Space>
+          </div>
+
+        </>
+      )}
+
+    </>
+  );
+}
 
 export default SearchBox;
